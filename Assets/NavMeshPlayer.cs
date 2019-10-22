5b516cff4cb3550b48f2a017af9c0b325d442293
arri <ewws@mail.ru>
2019-10-22 20:23:41 +0900

    added nav mesh

---
+++ Assets/NavMeshPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshPlayer : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private Camera mainCam;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 10000.0f))
+            {
+                Debug.Log(hit.point);
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
