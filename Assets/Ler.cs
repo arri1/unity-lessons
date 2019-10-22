6b3d2fce1f0dee7dc24894ad1844048021265406
arri <ewws@mail.ru>
2019-10-22 19:47:07 +0900

    initial commit

---
+++ Assets/Ler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var a = 1;
+        var b = 2;
+        var c = a-b;
+        Debug.Log(c);
+        if (c>0) Debug.Log("c polozhitelnoe");
+        else
+            Debug.Log("c 0 ili otrcatelnoe");
+        var d=new int[5]{0,1,2,3,4};
+        Debug.Log(d[0]);
+        int sum=0;
+        for(int i = 0;i<5;i++)
+        {
+            sum=sum+d[i];
+        }
+        Debug.Log(sum);
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/TestParticle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestParticle : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem partical;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        Debug.Log("wqeqw");
+    }
+}
